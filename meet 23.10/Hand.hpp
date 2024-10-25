e7603a3bc186952273cf22626030497bf72bcf33
Top <Top@8-18>
2024-10-25 18:36:31 +0500

    2.0

--- meet 23.10/Hand.hpp
+++ meet 23.10/Hand.hpp
@@ -1,4 +1,20 @@
 #pragma once
+#ifndef HAND_HPP
+#define HAND_HPP
+#include "Card.hpp"
+#include <vector>
 
-class Hand;
 
+namespace CardGame {
+	class Hand {
+		
+	public:
+
+	private:
+		std::vector<Card> cards_;
+
+	};
+
+}
+
+#endif
